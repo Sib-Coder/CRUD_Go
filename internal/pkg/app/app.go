abbbfde0e44da4f3c66371536730748c645bfadf
Sinitsyn Daniil <80570939+Sib-Coder@users.noreply.github.com>
2023-07-13 17:06:42 +0700

    update readme

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -29,6 +29,12 @@ func New() (*App, error) {
 	app.endpoint = endpoint.New(app.service)
 	app.echo = echo.New()
 
+	app.echo.GET("/", app.endpoint.Hello)
+	app.echo.GET("/user", app.endpoint.UserTake)
+	app.echo.POST("/user", app.endpoint.UserAdd)
+	app.echo.PUT("/user", app.endpoint.UserUpdate)
+	app.echo.DELETE("/user", app.endpoint.UserDelete)
+	app.echo.GET("/users", app.endpoint.UserSTake)
 	return app, nil
 }
 func (a *App) Run() error {
